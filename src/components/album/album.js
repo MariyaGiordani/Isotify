9bce6c84cee50d8227a9379e6621ee660b8ea13e
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-10-16 18:54:07 -0300

    added small albums component just for a simple test/to learn

---
+++ src/components/album/album.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./album.css";
+
+
+const Album = () => (
+    
+    <div className="card">
+        <img src="" className="card__cover card__cover--big"/>
+        <div className="card__info">
+          <div className="text">
+            <div className="text__title text__title--">sdas</div>
+            <div className="text__singer text__singer--">asdsa</div>
+          </div>
+          <div className="options">
+          </div>
+        </div>
+    </div>
+);
+
+export default Album;
--- src/views/Albums/list.js
+++ src/views/Albums/list.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Album from '../../components/album/album';
 
 const AlbumsList = () => (
   <div>
     <h1>ALBUMS</h1>
     <Link to="/artists">Artists</Link>
+    <Album/>
   </div>
 );
 
