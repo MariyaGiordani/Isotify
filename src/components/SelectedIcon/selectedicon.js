5f472cf6ec45726378866d011cbae8c3950d59de
Mariya Sergeeva Lukanova Giordani <mariyagiordani@mimoza.americas.media.global.loc>
2018-10-17 16:41:09 -0300

    Uploading new file which wasn't uploaded before.

---
+++ src/components/SelectedIcon/selectedicon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./selectedicon.css";
+
+const SelectedIcon = () => (
+    <div className="selected-icon"></div>
+);
+
+export default SelectedIcon;
---
+++ src/components/Sidebar/constants.js
@@ -0,0 +1,15 @@
+import headphoneImage from "../../assets/img/headphone.png";
+import musicImage from "../../assets/img/music.png";
+
+export const LINKS = [
+    {
+      name: "Albums",
+      to: `/albums`,
+      src: musicImage
+    },
+    {
+      name: "Artists",
+      to: `/artists`,
+      src: headphoneImage
+    }
+]
