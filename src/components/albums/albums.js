0696caa44dc6916301e49e45bc84b3b84b291a2a
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-10-18 17:43:15 -0300

    changed map call to a function, for better readability

--- src/components/albums/albums.js
+++ src/components/albums/albums.js
@@ -2,11 +2,20 @@ import React from "react";
 import Album from "../album/album";
 import "./albums.css";
 
+const getAlbumsComponents = (albums, size) => {
+  albums &&
+    albums.map((album) => (
+      <Album
+        title={album.title}
+        author={album.author}
+        size={size}
+        imgSrc={album.imgSrc}
+      />
+    ));
+};
+
 const Albums = ({ albums, size }) => (
-  <div className="albums-container">
-    {albums &&
-      albums.map((album) => <Album title={album.title} author={album.author} size={size} imgSrc={album.imgSrc} />)}
-  </div>
+  <div className="albums-container">{getAlbumsComponents(albums, size)}</div>
 );
 
 export default Albums;
