dedb01f0d5fa85f5ee0b531d651d44a68152162b
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-10-18 16:49:51 -0300

    destructuring in album component

--- src/components/album/album.js
+++ src/components/album/album.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./album.css";
 
-const Album = (props) => (
+const Album = ({ imgSrc, size, title, author }) => (
   <div className="album">
-    <img src={props.imgSrc} className={"album__cover album__cover--" + props.size} />
+    <img src={imgSrc} className={"album__cover album__cover--" + size} />
     <div className={"album__info"}>
       <div className="text">
-        <div className={"text__title"}>{props.title}</div>
-        <div className={"text__singer"}>{props.author}</div>
+        <p className={"text__title"}>{title}</p>
+        <p className={"text__singer"}>{author}</p>
       </div>
       <div className="options" />
     </div>
