4f0f5404f8a29ed28c82d0659f379140af17b5ce
EngelFlores <engel.flores@iwsbrazil.com>
2018-10-19 14:34:31 -0300

    Changing classes names

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -3,15 +3,17 @@ import { Link } from 'react-router-dom';
 import './index.css';
 
 const Login = () => (
-    <div className="content-login">
-        <div className="content-login__container-title">
-            <h1 className="container-title__phrase">Login</h1>
-            <button className="container-title__button">Login with Spotify</button>
-        </div>
-        <Link className="content-login__first-link" to="/">Remember Password?</Link>
-        <div className="content-login__container-text">
-            <Link className="content-login__second-link" to="/">didn’t do this YET?</Link>
-            <Link className="content-login__third-link" to="/">Sign up</Link>
+    <div className="content-background">
+        <div className="content-login">
+            <div className="content-login__container-title">
+                <h1 className="container-title__phrase">Login</h1>
+                <button className="container-title__button">Login with Spotify</button>
+            </div>
+            <Link className="content-login__first-link" to="/">Remember Password?</Link>
+            <div className="content-login__container-text">
+                <Link className="content-login__second-link" to="/">didn’t do this YET?</Link>
+                <Link className="content-login__third-link" to="/">Sign up</Link>
+            </div>
         </div>
     </div>
 );
