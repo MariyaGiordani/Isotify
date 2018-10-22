76acb79c1a09ed3d53060cbd91ced1561ec2d478
EngelFlores <engel.flores@iwsbrazil.com>
2018-10-22 17:20:42 -0300

    Changing URI

--- src/services/spotify/getClientInfo.js
+++ src/services/spotify/getClientInfo.js
@@ -2,7 +2,8 @@ import { generateRandomString } from './generateRandomString';
 export function getClientInfo() {
     let stateKey = "spotify_auth_state"
     let client_id = process.env.REACT_APP_API_KEY; // Your client id
-    let redirect_uri = process.env.REACT_APP_API_URI; // Your redirect uri
+    let redirect_uri = process.env.REACT_APP_API_URI + "albums/"; // Your redirect uri
+    console.log(redirect_uri );
     let state = generateRandomString(16);
     localStorage.setItem(stateKey, state);
     let scope = "user-read-private user-read-email";
