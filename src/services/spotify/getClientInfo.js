cfbe073de128917a28b750101bf3d09b713f0e84
EngelFlores <engel.flores@iwsbrazil.com>
2018-10-22 16:16:51 -0300

    Removing console.log

--- src/services/spotify/getClientInfo.js
+++ src/services/spotify/getClientInfo.js
@@ -1,6 +1,5 @@
 import { generateRandomString } from './generateRandomString';
 export function buttonEvent() {
-    console.log("pqp")
     let stateKey = "spotify_auth_state"
     let client_id = "b0348f3e529b4f04a92f0ae70a598c25"; // Your client id
     let redirect_uri = "http://localhost:3000/albums/"; // Your redirect uri
