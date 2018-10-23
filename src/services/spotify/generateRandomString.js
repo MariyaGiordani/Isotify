4ec4e7299637df586e50fb57b2b9261b9a639b53
Afonso Ferrer <afonsoferrer@fonziescomputer.americas.media.global.loc>
2018-10-23 11:31:26 -0300

    changed quotes in generateRandomString file to better adequate conventions

--- src/services/spotify/generateRandomString.js
+++ src/services/spotify/generateRandomString.js
@@ -1,9 +1,9 @@
 export function generateRandomString(length) {
-    let text = "";
-    let possible =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+  let text = '';
+  let possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
