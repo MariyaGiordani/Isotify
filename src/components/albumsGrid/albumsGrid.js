a309c7ccff07adf93d90239466715c71cf7b3c61
Afonso Ferrer <afonsoferrer@fonziescomputer.americas.media.global.loc>
2018-10-24 15:18:42 -0300

    removed complexity in code and changed single quote to double quotes

--- src/components/albumsGrid/albumsGrid.js
+++ src/components/albumsGrid/albumsGrid.js
@@ -2,19 +2,15 @@ import React from 'react';
 import Album from '../album/album';
 import './albumsGrid.css';
 
-const getAlbumsComponents = (albums, size) => {
-  return (
-    !!albums &&
-    albums.map((album) => (
-      <Album
-        title={album.title}
-        author={album.author}
-        size={size}
-        imgSrc={album.imgSrc}
-      />
-    ))
-  );
-};
+const getAlbumsComponents = (albums = [], size) =>
+  albums.map((album) => (
+    <Album
+      title={album.title}
+      author={album.author}
+      size={size}
+      imgSrc={album.imgSrc}
+    />
+  ));
 
 const AlbumsGrid = ({ albums, size }) => (
   <div className="albums-grid">{getAlbumsComponents(albums, size)}</div>
