3c23009298c6445f4359041b17ff01e564067a29
Mariya Sergeeva Lukanova Giordani <mariyagiordani@mimoza.americas.media.global.loc>
2018-10-24 17:50:10 -0300

    Created new branch IWSITS-12

--- src/views/Artists/list.js
+++ src/views/Artists/list.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import SideBar from "../../components/Sidebar/sidebar";
+import SideBar from '../../components/Sidebar/sidebar';
 
 const ArtistsList = () => (
   <div>
     <SideBar />
-    <div className="container">
-      Artists
-    </div>
+    <div className="container">Artists</div>
   </div>
 );
 
