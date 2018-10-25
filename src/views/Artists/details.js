d8bb7243becca78cc030dedb87391fe73d953340
Mariya Sergeeva Lukanova Giordani <mariyagiordani@mimoza.americas.media.global.loc>
2018-10-25 16:05:35 -0300

    Empty div in Artists details.js was changed to Fragment.

--- src/views/Artists/details.js
+++ src/views/Artists/details.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import BannerArtist from '../../components/BannerArtist/bannerArtist';
 import SideBar from '../../components/Sidebar/sidebar';
 
 const ArtistDetails = (props) => (
-  <div>
+  <Fragment>
     <SideBar />
     <div className="container">
       <BannerArtist />
       <h1>Artist {props.match.params.artistId}</h1>
       <Link to="/artists">Back to Artists</Link>
     </div>
-  </div>
+  </Fragment>
 );
 
 export default ArtistDetails;
