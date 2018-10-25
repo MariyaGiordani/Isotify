5c26bb789c1108ba8465f42b45a90de737bbfd8b
Mariya Sergeeva Lukanova Giordani <mariyagiordani@mimoza.americas.media.global.loc>
2018-10-25 15:49:28 -0300

    Removed div and replaced with fragment  src/views/Artists/list.js

--- src/views/Artists/list.js
+++ src/views/Artists/list.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import SideBar from '../../components/Sidebar/sidebar';
 
-const ArtistsList = () => {
-  console.log('here artists lists');
-  return (
-    <div>
-      <SideBar />
-      <div className="container">
-        <Link to="/artists/1">Artists</Link>
-      </div>
+const ArtistsList = () => (
+  <Fragment>
+    <SideBar />
+    <div className="container">
+      <Link to="/artists/1">Artists</Link>
     </div>
-  );
-};
+  </Fragment>
+);
 
 export default ArtistsList;
