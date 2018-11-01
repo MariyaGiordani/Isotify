58a9751ff756691f2c3b1168c0c2fa5afc3d2e1a
Mariya Sergeeva Lukanova Giordani <mariyagiordani@mimoza.americas.media.global.loc>
2018-11-01 13:31:52 -0300

    Header is ready, responsive of header  not ready yet.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import './header.css';
 
+import triangleImg from '../../assets/img/triangle.png';
+import playlisticonImg from '../../assets/img/playlisticon.png';
+import mountainImg from '../../assets/img/mountain.png';
+import Search from '../../components/Search/search';
+
 const Header = () => (
   <div className="header">
     <div className="header-icon--size">
@@ -9,8 +14,16 @@ const Header = () => (
     </div>
     <div className="header__wrap">
       <p className="header-user__name">Darya Vermalen</p>
-      <p className="header-user__text">VIEW PROFILE</p>
+      <a className="header-user__profile" href="#viewprofile">
+        VIEW PROFILE
+      </a>
     </div>
+    <img className="header__image-triangle" alt="" src={triangleImg} />
+    <div className="header__devider" />
+    <Search />
+    <img className="header__image-playlist" alt="" src={playlisticonImg} />
+    <div className="header__devider" />
+    <img className="header__image-mountain" alt="" src={mountainImg} />
   </div>
 );
 
