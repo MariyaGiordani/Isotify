59ea3ca8f3a800396754f5115ae554b6727cbcd1
Afonso Ferrer <afonso.ferrer@iwsbrazil.com>
2018-11-06 12:55:16 -0200

    removing line breaks and changing getAllAlbumsFromArtist to be more DRY

--- src/services/albums.js
+++ src/services/albums.js
@@ -10,29 +10,15 @@ function getAlbums(ids) {
 }
 
 function getAllAlbumsFromArtist(artistId) {
-  return spotifyInstance
-    .get(
-      `artists/${artistId}/albums?market=BR&include_groups=album,single&limit=50&`,
-      createHeader()
-    )
-    .then(function(response) {
-      const nextUrl = response.data.next;
-      return nextUrl
-        ? getAllAlbumsFromArtistRecursively(
-            response.data.items,
-            response.data.next
-          )
-        : response.data.items;
-    });
+  const nextUrl = `https://api.spotify.com/v1/artists/${artistId}/albums?market=BR&include_groups=album,single&limit=50&`;
+  const albums = [];
+  return getAllAlbumsFromArtistRecursively(albums, nextUrl);
 }
 
 function getAllAlbumsFromArtistRecursively(albums, nextUrl) {
   return axios.get(nextUrl, createHeader()).then(function(response) {
     if (response.data.next) {
-      return getAllAlbumsFromArtistRecursively(
-        albums.concat(response.data.items),
-        response.data.next
-      );
+      return getAllAlbumsFromArtistRecursively(albums.concat(response.data.items), response.data.next);
     } else {
       return albums.concat(response.data.items);
     }
