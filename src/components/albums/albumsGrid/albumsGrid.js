4eaa0f0c4d06037afff58720cb1f1923f7488a1a
Afonso Ferrer <afonso.ferrer@iwsbrazil.com>
2018-11-08 15:55:04 -0200

    also putting spread operator in albumsGrid

--- src/components/albums/albumsGrid/albumsGrid.js
+++ src/components/albums/albumsGrid/albumsGrid.js
@@ -3,10 +3,10 @@ import Album from '../album/album';
 import './albumsGrid.css';
 
 const getAlbumsComponents = (albums = [], size) =>
-  albums.map((album) => (
-    <Album title={album.title} artist={album.artist} size={size} imgSrc={album.imgSrc} key={album.id} />
-  ));
+  albums.map((album) => <Album {...{ ...album, size, key: album.id }} />);
 
-const AlbumsGrid = ({ albums, size }) => <div className="albums-grid">{getAlbumsComponents(albums, size)}</div>;
+const AlbumsGrid = ({ albums, size }) => (
+  <div className="albums-grid">{getAlbumsComponents(albums, size)}</div>
+);
 
 export default AlbumsGrid;
