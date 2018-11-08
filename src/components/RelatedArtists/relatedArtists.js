0c091dd45e7b8229a082ee3cfe3f1206341e84f9
Afonso Ferrer <afonso.ferrer@iwsbrazil.com>
2018-11-08 15:28:39 -0200

    changed alt of related artists

--- src/components/RelatedArtists/relatedArtists.js
+++ src/components/RelatedArtists/relatedArtists.js
@@ -5,13 +5,15 @@ import RelatedArtistsIcon from '../RelatedArtistsIcon/relatedArtistsIcon';
 import RelatedArtistsMore from '../RelatedArtistsMore/relatedArtistsMore';
 
 import './relatedArtists.css';
-const RelatedArtists = (props) => {
-  const artistsIds = props.artists.map((artist) => artist.id);
+const RelatedArtists = ({ artists }) => {
+  const artistsIds = artists.map((artist) => artist.id);
 
   const listIcons = () => {
-    return props.artists
+    return artists
       .slice(0, 4)
-      .map((artist) => <RelatedArtistsIcon key={artist.id} src={artist.imgSrc} id={artist.id} />);
+      .map((artist) => (
+        <RelatedArtistsIcon key={artist.id} src={artist.imgSrc} id={artist.id} artistName={artist.name} />
+      ));
   };
 
   return (
@@ -20,7 +22,7 @@ const RelatedArtists = (props) => {
       <p className="related-artists__text">RELATED ARTISTS</p>
       <div className="related-artists__wrap">
         {listIcons()}
-        {props.artists.length >= 4 && (
+        {artists.length >= 4 && (
           <Link to={`/artists/related/${artistsIds}`}>
             <RelatedArtistsMore />
           </Link>
--- src/components/RelatedArtistsIcon/relatedArtistsIcon.js
+++ src/components/RelatedArtistsIcon/relatedArtistsIcon.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './relatedArtistsIcon.css';
 
-const RelatedArtistsIcon = ({ src, id }) => {
+const RelatedArtistsIcon = ({ src, id, artistName }) => {
   return (
     <Link to={`/artists/${id}`} className="related-artists-icon">
-      <img className="related-artists-icon__image" alt="Related artists" src={src} />
+      <img className="related-artists-icon__image" alt={`Related: ${artistName}`} src={src} />
     </Link>
   );
 };
