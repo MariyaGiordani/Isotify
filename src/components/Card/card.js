b60340e4d9a213a5871f29251c8f89bf410d75e7
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-11-12 18:59:24 -0200

    still fixing things

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -4,17 +4,17 @@ import './card.css';
 
 const createLink = (link, element) => <Link to={link}>{element}</Link>;
 
-const Card = ({ imgSrc, size, title, subtitle }) => {
-  const titleParagraph = <p className="card__text-title">{title.text}</p>;
-  const subtitleParagraph = <p className="card__text-singer">{subtitle.text}</p>;
+const Card = ({ imgSrc, size, title, subtitle, titleHref, subtitleHref }) => {
+  const titleParagraph = <p className="card__text-title">{title}</p>;
+  const subtitleParagraph = <p className="card__text-singer">{subtitle}</p>;
 
   return (
     <div className={`card card--${size}`}>
       <img src={imgSrc} alt={title} className="card__cover" />
       <div className="card__info">
         <div className="card__text">
-          {title.link ? createLink(title.link, titleParagraph) : titleParagraph}
-          {subtitle.link ? createLink(subtitle.link, subtitleParagraph) : subtitleParagraph}
+          {titleHref.link ? createLink(title.link, titleParagraph) : titleParagraph}
+          {subtitleHref.link ? createLink(subtitle.link, subtitleParagraph) : subtitleParagraph}
         </div>
         <div className="card__options" />
       </div>
--- src/components/albums/albumsGrid/albumsGrid.js
+++ src/components/albums/albumsGrid/albumsGrid.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Card from '../../Card/card';
 import './albumsGrid.css';
 
+const artistLink = (artist) => ({ text: artist.name, link: `/artists/${artist.id}` });
+const albumYearToArtistLink = (album) => ({
+  text: album.date.slice[0 - 4],
+  link: `/artists/${album.artist.id}`
+});
+
 const getAlbumsComponents = (albums = [], size) =>
   albums.map(({ imgSrc, title, artist, id }) => (
-    <Card
-      imgSrc={imgSrc}
-      size={size}
-      title={{ text: title }}
-      subtitle={{ text: artist.name, link: `/artists/${artist.id}` }}
-      key={id}
-    />
+    <Card imgSrc={imgSrc} size={size} title={{ text: title }} subtitle={artistLink(artist)} key={id} />
   ));
 
 const AlbumsGrid = ({ albums, size }) => (
