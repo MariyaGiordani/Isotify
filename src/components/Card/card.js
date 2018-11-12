dd2ffedd191c9f2fda44fb37a70f9af3d51a2b47
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-11-12 17:50:07 -0200

    finished changing album component to more generic card component

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -2,21 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './card.css';
 
-const createLink = (text, link) => <Link to={link}>{createSubtitle(text)}</Link>;
-
+const createLink = (link, element) => <Link to={link}>{element}</Link>;
+const createTitle = (text) => <p className="card__text-title">{text}</p>;
 const createSubtitle = (text) => <p className="card__text-singer">{text}</p>;
 
-const Card = ({ imgSrc, size, title, subtitle, link }) => (
-  <div className={`card card--${size}`}>
-    <img src={imgSrc} alt={title} className="card__cover" />
-    <div className="card__info">
-      <div className="card__text">
-        <p className="card__text-title">{title}</p>
-        {link ? createLink(subtitle, link) : createSubtitle(subtitle)}
+const Card = ({ imgSrc, size, title, subtitle }) => {
+  const titleParagraph = createTitle(title.text);
+  const subtitleParagraph = createSubtitle(subtitle.text);
+  return (
+    <div className={`card card--${size}`}>
+      <img src={imgSrc} alt={title} className="card__cover" />
+      <div className="card__info">
+        <div className="card__text">
+          {title.link ? createLink(title.link, titleParagraph) : titleParagraph}
+          {subtitle.link ? createLink(subtitle.link, subtitleParagraph) : subtitleParagraph}
+        </div>
+        <div className="card__options" />
       </div>
-      <div className="card__options" />
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
--- src/components/albums/albumsGrid/albumsGrid.js
+++ src/components/albums/albumsGrid/albumsGrid.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Album from '../album/album';
 import Card from '../../Card/card';
 import './albumsGrid.css';
 
 const getAlbumsComponents = (albums = [], size) =>
-  albums.map((album) => <Album {...{ ...album, size, key: album.id }} />);
+  albums.map(({ imgSrc, title, artist, id }) => (
+    <Card
+      imgSrc={imgSrc}
+      size={size}
+      title={{ text: title }}
+      subtitle={{ text: artist.name, link: `/artists/${artist.id}` }}
+      key={id}
+    />
+  ));
 
 const AlbumsGrid = ({ albums, size }) => (
   <div className="albums-grid">{getAlbumsComponents(albums, size)}</div>
