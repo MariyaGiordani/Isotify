61b5a579fc82f7ad370d1520bb6add6a68452c0f
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-11-22 12:54:56 -0200

    using fragment and being more fail safe

--- src/components/TopSongsAndArtists/topSongsAndArtists.js
+++ src/components/TopSongsAndArtists/topSongsAndArtists.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import songIcon from '../../assets/img/headphone-song.svg';
 import TopItem from './topItem';
 
 import './topSongsAndArtists.css';
 
 const topArtists = (artists) => (
-  <div>
+  <Fragment>
     <h2 className="top-songs-and-artists__header">Top Artists</h2>
     {artists.map((artist, index) => (
       <TopItem icon={songIcon} title={artist.name} subtitle={artist.genre} hasDivisor={index !== 4} />
     ))}
-  </div>
+  </Fragment>
 );
 
 const topSongs = (songs) => (
-  <div>
+  <Fragment>
     <h2 className="top-songs-and-artists__header">Top Songs</h2>
     {songs.map((song, index) => (
       <TopItem icon={songIcon} title={song.songName} subtitle={song.artist} hasDivisor={index !== 4} />
     ))}
-  </div>
+  </Fragment>
 );
 
-const TopSongsAndArtists = ({ songs, artists }) => (
+const TopSongsAndArtists = ({ songs = [], artists = [] }) => (
   <div className="top-songs-and-artists">
     {topSongs(songs)}
     <div className="top-songs-and-artists__line" />
