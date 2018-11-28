97f463b76492cfea8cfa5d2379d0dadc2cb870d8
Mariya Sergeeva Lukanova Giordani <mariyagiordani@mimoza.americas.media.global.loc>
2018-11-28 11:38:57 -0200

    carouselArtists: carouselArtists, change to carouselArtists,

--- src/views/Discover/index.js
+++ src/views/Discover/index.js
@@ -23,7 +23,7 @@ export default class List extends Component {
         })
       );
       this.setState({
-        carouselArtists: carouselArtists,
+        carouselArtists,
         albums
       });
     });
