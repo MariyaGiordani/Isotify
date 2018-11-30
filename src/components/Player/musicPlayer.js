1978808eb56a6a5e3d41723dbd949a4de0a48c63
EngelFlores <engel.flores@iwsbrazil.com>
2018-11-30 15:06:45 -0200

    Changing volume icon and variables-colors

--- src/components/Player/musicPlayer.js
+++ src/components/Player/musicPlayer.js
@@ -10,40 +10,51 @@ import volume from '../../assets/img/speaker.png';
 export default () => (
   <div className="player">
     <div className="player__container">
-      <div className="container__playlisticon">
-        <img
-          className="playlisticon__image"
-          alt="Playlisticon"
-          src={playlisticon}
-        />
-      </div>
-      <div className="playlisticon__music-info">
+      <button className="container__button">
+        <div className="container__playlist-icon">
+          <img
+            className="playlist-icon__image"
+            alt="Playlist-icon"
+            src={playlisticon}
+          />
+        </div>
+      </button>
+
+      <div className="playlist-icon__music-info">
         <h1 className="music-info__name">Nome da música</h1>
         <h2 className="music-info__band">Nome da banda</h2>
       </div>
     </div>
 
     <div className="player__buttons">
-      <div className="buttons__prev-container">
-        <img className="prev-container__image" alt="Prev Button" src={prev} />
-      </div>
-      <div className="buttons__pause-container">
+      <button className="buttons__prev">
+        <div className="buttons__prev-container">
+          <img className="prev-container__image" alt="Prev Button" src={prev} />
+        </div>
+      </button>
+      <button className="buttons__pause">
+        <div className="buttons__pause-container">
+          <img
+            className="pause-container__image"
+            alt="Pause Button"
+            src={pause}
+          />
+        </div>
+      </button>
+      <button className="buttons__next">
+        <div className="buttons__next-container">
+          <img className="next-container__image" alt="Next Button" src={next} />
+        </div>
+      </button>
+    </div>
+    <button className="player__volume">
+      <div className="player__volume-container">
         <img
-          className="pause-container__image"
-          alt="Pause Button"
-          src={pause}
+          className="volume-container__image"
+          alt="Playlist-icon"
+          src={volume}
         />
       </div>
-      <div className="buttons__next-container">
-        <img className="next-container__image" alt="Next Button" src={next} />
-      </div>
-    </div>
-    <div className="player__volume-container">
-      <img
-        className="volume-container__image"
-        alt="Playlisticon"
-        src={volume}
-      />
-    </div>
+    </button>
   </div>
 );
