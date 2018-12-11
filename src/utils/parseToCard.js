5df893dd698d720b1930dee7a1f95f75fad3aa45
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-12-11 12:31:28 -0200

    refactored parse to card

--- src/utils/parseToCard.js
+++ src/utils/parseToCard.js
@@ -1,30 +1,66 @@
 const getSongsComponents = (tracks = []) =>
-  tracks.map(({ albumImage, songName, artist, id, size }) => ({
-    imgSrc: albumImage,
-    title: songName,
-    subtitle: artist.name,
-    key: id,
-    size: size
-  }));
+  tracks.map(
+    ({
+      albumImage,
+      songName,
+      artist,
+      id,
+      size,
+      titleHref = '',
+      subtitleHref = '',
+      popup = {},
+      hasHover = false,
+      hoverCallback = () => {}
+    }) => ({
+      imgSrc: albumImage,
+      title: songName,
+      subtitle: artist.name,
+      key: id,
+      size,
+      titleHref,
+      subtitleHref,
+      popup,
+      hasHover,
+      hoverCallback
+    })
+  );
 
 const getArtistsComponents = (artists = []) =>
-  artists.map(({ imgSrc, name, id, albums, totalTracks, size }) => ({
-    imgSrc: imgSrc,
-    size: size,
-    title: name,
-    subtitle: `${albums.length} Albums, ${totalTracks} Songs`,
-    titleHref: `/artists/${id}`,
-    key: id
-  }));
+  artists.map(
+    ({
+      imgSrc,
+      name,
+      id,
+      albums,
+      totalTracks,
+      size,
+      subtitleHref = '',
+      popup = {},
+      hasHover = false,
+      hoverCallback = () => {}
+    }) => ({
+      imgSrc,
+      size,
+      title: name,
+      subtitle: `${albums.length} Albums, ${totalTracks} Songs`,
+      titleHref: `/artists/${id}`,
+      key: id,
+      subtitleHref,
+      popup,
+      hasHover,
+      hoverCallback
+    })
+  );
 
 const getPlaylistComponents = (playlist = []) =>
   playlist.map(
     ({ imagePlaylist, size, namePlaylist, followersPlaylist, id }) => ({
       imgSrc: imagePlaylist,
-      size: size,
+      size,
       title: namePlaylist,
       subtitle: `${followersPlaylist} followers`,
-      key: id
+      key: id,
+      id
     })
   );
 
