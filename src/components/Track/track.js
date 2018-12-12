bd562f4265d3372cdb491a012f3202ec7f6eafbc
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-12-12 18:21:14 -0200

    removed stateless component

--- src/components/Track/track.js
+++ src/components/Track/track.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Card from '../../components/Card/card';
 import { PlayerContext } from '../../components/Player/musicPlayer';
 
-class Track extends Component {
-  render = () => {
-    const {
-      albumImage: imgSrc = '',
-      size,
-      songName: title,
-      artist,
-      id
-    } = this.props;
-    return (
-      <PlayerContext>
-        {(context) => (
-          <Card
-            {...{
-              imgSrc,
-              size,
-              title,
-              id,
-              subtitle: artist.name,
-              subtitleHref: `/artists/${artist.id}`,
-              key: id,
-              hasHover: true,
-              hoverCallback: (id) => context.onClickPlayTrack([id])
-            }}
-          />
-        )}
-      </PlayerContext>
-    );
-  };
-}
-
-export default Track;
+export default ({
+  albumImage: imgSrc = '',
+  size,
+  songName: title,
+  artist,
+  id
+}) => (
+  <PlayerContext>
+    {(context) => (
+      <Card
+        {...{
+          imgSrc,
+          size,
+          title,
+          id,
+          subtitle: artist.name,
+          subtitleHref: `/artists/${artist.id}`,
+          key: id,
+          hasHover: true,
+          hoverCallback: (id) => context.onClickPlayTrack([id])
+        }}
+      />
+    )}
+  </PlayerContext>
+);
