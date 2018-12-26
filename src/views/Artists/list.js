324016c27f6a8e5889aad4e2da55747a3d213b7f
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2018-12-26 16:42:34 -0200

    added loader

--- src/views/Artists/list.js
+++ src/views/Artists/list.js
@@ -3,10 +3,12 @@ import ArtistsList from '../../components/artists/ArtistsList/ArtistsList';
 import ArtistsGrid from '../../components/artists/ArtistsGrid/ArtistsGrid';
 import HeaderLine from '../../components/headerLine/headerLine';
 import SwitchButton from '../../components/SwitchButton/switchButton';
+import Spinner from '../../components/Spinner/spinner';
 import PageContainer from '../../components/PageContainer/pageContainer';
 import { getTopArtistsWithAlbums } from '../../services/artists';
 import { artistsWithAlbums as parseTopArtists } from '../../utils/spotifyResponseParsers';
 import { serverError } from '../../utils/errors';
+
 import './Artists.css';
 
 export default class ArtistsListView extends Component {
@@ -52,10 +54,12 @@ export default class ArtistsListView extends Component {
       error
     } = this.state;
     const subtitle = `${artistsAmount} Artists, ${songsAmount} Songs`;
-
+    const isPageLoading = artists.length === 0;
     return (
       <PageContainer>
-        {error || (
+        {error || isPageLoading ? (
+          <Spinner />
+        ) : (
           <Fragment>
             <HeaderLine
               {...{
