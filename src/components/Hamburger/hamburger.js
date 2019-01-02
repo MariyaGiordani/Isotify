e41d723f46caef60c43635add345a24d46b172d8
Afonso Ferrer <afonsoferrer@FonziesComputer.lan>
2019-01-02 09:45:18 -0200

    fixed dark theme for hamburger icon

--- src/components/Hamburger/hamburger.js
+++ src/components/Hamburger/hamburger.js
@@ -4,9 +4,9 @@ import './hamburger.css';
 const Hamburger = ({ hamburgerClick }) => (
   <button className="hamburger-menu" onClick={hamburgerClick}>
     <svg width="30" height="30">
-      <path d="M0,5 30,5" stroke="var(--black)" strokeWidth="4" />
-      <path d="M0,14 30,14" stroke="var(--black)" strokeWidth="4" />
-      <path d="M0,23 30,23" stroke="var(--black)" strokeWidth="4" />
+      <path d="M0,5 30,5" stroke="var(--theme-hamburger)" strokeWidth="4" />
+      <path d="M0,14 30,14" stroke="var(--theme-hamburger)" strokeWidth="4" />
+      <path d="M0,23 30,23" stroke="var(--theme-hamburger)" strokeWidth="4" />
     </svg>
   </button>
 );
